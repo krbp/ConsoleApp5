1e2172ecdfef299fe38221f11bde8a05768b3894
krbp <165799825+krbp@users.noreply.github.com>
2024-06-11 12:24:36 +0200

    Add project files.

---
+++ CustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class CustomException : Exception
+    {
+        public DateTime Time { get; }
+        public CustomException(string message) : base(message)
+        {
+            Time = DateTime.Now;
+        }
+
+        public static void ThrowError(DateTime t)
+        {
+            throw new CustomException($"Time: {t} ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CustomException.ThrowError(DateTime.Now);
+            }
+            catch(CustomException e)
+            {
+                Console.WriteLine($"{e}");
+            }
+        }
+    }
+}
